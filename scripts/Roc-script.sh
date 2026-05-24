da3711f0bd98331f8570960467929a2e56a8fa47
ybjbox <103187857+ybjbox@users.noreply.github.com>
2026-05-24 10:54:13 +0800

    Configure wireless settings for 2.4G radio
    Enable all wireless radios and configure 2.4G to use VHT40 mode and Wi-Fi 5 support.

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -141,10 +141,17 @@ while uci get wireless.@wifi-iface[\$wireless_idx] >/dev/null 2>&1; do
     wireless_idx=\$((\$wireless_idx + 1))
 done
 
-# 启用所有无线网卡
+# 启用所有无线网卡，并配置 2.4G 默认为 ac (VHT / Wi-Fi 5) 信号
 radio_idx=0
 while uci get wireless.radio\$radio_idx >/dev/null 2>&1; do
     uci set wireless.radio\$radio_idx.disabled='0'
+    
+    # 如果是 radio0 (通常为设备的 2.4G 物理网卡)
+    if [ "\$radio_idx" = "0" ]; then
+        uci set wireless.radio0.htmode='VHT40'      # 开启 2.4G 频段的 VHT40 模式 (802.11ac 2.4G 扩展)
+        uci set wireless.radio0.ieee80211ac='1'     # 开启 ieee80211ac (Wi-Fi 5) 属性支持
+    fi
+    
     radio_idx=\$((\$radio_idx + 1))
 done
 
